f4c32489d651a9436cf23979b403a04adc71dc95
coqkr <ycoqkr@gmail.com>
2020-07-07 14:12:55 +0900

    list 사용 가능 함수

---
+++ list-prac.py
@@ -0,0 +1,30 @@
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+
+print(subway.index("조세호"))
+
+subway.append("하하")
+print(subway)
+
+# 유재석과 조세호 사이에 정형돈을 태움
+subway.insert(1, "정형돈")
+print(subway)
+
+# 뒤에서부터 꺼내기
+
+print(subway.pop())
+print(subway)
+
+# 정렬하기
+
+num_list = [5, 2, 4, 3, 1]
+num_list.sort()
+print(num_list)
+
+# 순서 뒤집기
+num_list.reverse()
+print(num_list)
+
+# 리스트 모두 지우기
+num_list.clear()
+print(num_list)
