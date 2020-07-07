1f934585a5799657448ba748f8e911e9ffd6f5c3
coqkr <ycoqkr@gmail.com>
2020-07-07 13:57:14 +0900

    해답추가

--- quiz/quiz3.py
+++ quiz/quiz3.py
@@ -24,3 +24,12 @@ print(temp)
 pwd = temp[:3] + str(len(temp)) + str(temp.count("e")) + "!"
 
 print(pwd)
+
+
+# 해답
+
+url = "http://naver.com"
+my_str = url.replace("http://", "")
+my_str = my_str[:my_str.index(".")]
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0} 의 비밀번호는 {1} 입니다.".format(url, password))
