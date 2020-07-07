804deb9146904f19c387ddd7c955190f385bfcc4
coqkr <ycoqkr@gmail.com>
2020-07-07 14:14:40 +0900

    extend 기능 추가

--- list-prac.py
+++ list-prac.py
@@ -28,3 +28,10 @@ print(num_list)
 # 리스트 모두 지우기
 num_list.clear()
 print(num_list)
+
+# 다양한 자료형 함께 사용 및 리스트 확장
+num_list = [5, 2, 4, 3, 1]
+mix_list = ["조세호", 20, False]
+
+num_list.extend(mix_list)
+print(num_list)
