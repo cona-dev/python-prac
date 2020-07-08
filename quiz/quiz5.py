6a9cde559f439e02ad59d79bf0bc64f32707bec7
coqkr <ycoqkr@gmail.com>
2020-07-08 16:03:45 +0900

    quiz5 solution

--- quiz/quiz5.py
+++ quiz/quiz5.py
@@ -15,3 +15,15 @@ Quiz5) 당신은 Cocoa 서비스를 이용하는 택시 기사님입니다.
 총 탑승 고객 : 2 분
 """
 
+from random import *
+
+cnt = 0
+for waiting_no in range(1, 51): # 1, 2, 3, 4, 5 ~ 50
+    wait_time = randint(5, 50)
+    if wait_time >= 5 and wait_time <=15: 
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(waiting_no,wait_time))
+        cnt += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(waiting_no,wait_time))
+
+print("\n총 탑승 승객 : {}분".format(cnt))
