eb3dce773d544629d0f520761badd0b9adc955ef
coqkr <ycoqkr@gmail.com>
2020-07-08 16:42:57 +0900

    global variable or local variable

--- function-prac.py
+++ function-prac.py
@@ -6,3 +6,27 @@ def profile(name, age, *language):
 
 profile("chaeyeon", 24, "Python", "Java", "JavaScript", "Html", "css")
 profile("jinho", 27)
+
+# ---------------------------------------------------------------------------
+
+gun = 10
+
+#local gun
+def checkpoint(soldiers):
+    gun = 20
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+
+#global gun
+def checkpoint2(soldiers):
+    global gun
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+
+# print("전체 총 : {0}".format(gun))
+# checkpoint(2)
+# print("남은 총 : {0}".format(gun))
+
+print("전체 총 : {0}".format(gun))
+checkpoint2(2)
+print("남은 총 : {0}".format(gun))
