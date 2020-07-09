3581fce8f2c5dd50dee9d8906c9c4d2949a1c9bc
cona-dev <ycoqkr@gmail.com>
2020-07-09 15:36:25 +0900

    나도코딩 정답 코드

--- quiz/quiz8.py
+++ quiz/quiz8.py
@@ -23,6 +23,16 @@ class House:
     def show_detail(self):
         print("{} {} {} {} {}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
 
-a = House("강남", "아파트", "매매", "10억", "2010년")
-a.show_detail()
+# a = House("강남", "아파트", "매매", "10억", "2010년")
+# a.show_detail()
 
+# list 사용 - 강좌
+
+house = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "매매", "500/50", "2000년")
+
+house.append(house1)
+house.append(house2)
+house.append(house3)
