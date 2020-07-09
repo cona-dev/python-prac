b17b1b8d020d520007f92195a23bdf5ff97cb2c1
cona-dev <ycoqkr@gmail.com>
2020-07-09 14:18:33 +0900

    super() method Add

---
+++ starcraft/starcraft5-prac.py
@@ -0,0 +1,50 @@
+# normal unit
+class Unit:
+    def __init__(self, name, hp, speed):
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{} : {} 방향으로 이동합니다. [속도 {}]".format(self.name, location, self.speed))
+        
+# attack unit상속 받음
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed) # 상속
+        self.damage = damage
+        
+    
+    def attack(self, location):
+        print("{} : {} 방향으로 적군을 공격합니다. [공격력 {}]".format(self.name, location, self.damage))
+    
+    def damaged(self, damage):
+        print("{} : {} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{} : 현재 체력은 {} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{} : 파괴되었습니다.".format(self.name))
+    
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+    def fly(self, name, location):
+        print("{} : {} 방향으로 날아갑니다. [속도 {}]".format(name, location, self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상  speed 0
+        Flyable.__init__(self, flying_speed)
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0) #super로 부모 객체를 상속받음 self 사용 XX
+        self.location = location
+
