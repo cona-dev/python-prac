300445840cfed7ddd22a386498efd455e4e73ecb
cona-dev <ycoqkr@gmail.com>
2020-07-09 13:54:30 +0900

    상속을 이용해 함수를 재정의

--- starcraft4-prac.py
+++ starcraft4-prac.py
@@ -1,13 +1,17 @@
 # normal unit
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{} : {} 방향으로 이동합니다. [속도 {}]".format(self.name, location, self.speed))
         
 # attack unit상속 받음
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self, name, hp) # 상속
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed) # 상속
         self.damage = damage
         
     
@@ -18,15 +22,33 @@ class AttackUnit(Unit):
         print("{} : {} 데미지를 입었습니다.".format(self.name, damage))
         self.hp -= damage
         print("{} : 현재 체력은 {} 입니다.".format(self.name, self.hp))
-
         if self.hp <= 0:
             print("{} : 파괴되었습니다.".format(self.name))
     
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+    def fly(self, name, location):
+        print("{} : {} 방향으로 날아갑니다. [속도 {}]".format(name, location, self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상  speed 0
+        Flyable.__init__(self, flying_speed)
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+vulture = AttackUnit("vulture", 80, 10, 20)
+
+battlecruiser = FlyableAttackUnit("battlecruiser", 500, 25, 3)
+
+vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")
 
 
-firebat1 = AttackUnit("firebat", 50, 16)
-firebat1.attack("5시")
-
-firebat1.damaged(25)
-firebat1.damaged(25)
-
+valkyrie = FlyableAttackUnit("Valkyrie", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
