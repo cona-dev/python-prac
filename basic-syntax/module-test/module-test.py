9fa2c00e15ef6ebb607e75bb5a5a9c8855d36dd6
cona-dev <ycoqkr@gmail.com>
2020-07-10 09:23:45 +0900

    module 약어 사용방법

--- basic-syntax/module-test/module-test.py
+++ basic-syntax/module-test/module-test.py
@@ -1,1 +1,11 @@
-import theater-module
+# import theater_module
+
+# theater_module.price(3) #3명이서 영화보러 갔을 때 가격
+# theater_module.price_morning(4) # 4명이서 조조 할인 보러 갔을 때
+# theater_module.price_soldier(5) # 5명 군인
+
+# module 약어 사용
+import theater_module as mv
+mv.price(3)
+mv.price_morning(4)
+mv.price_soldier(5)
