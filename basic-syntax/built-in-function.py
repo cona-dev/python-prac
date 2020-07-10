2c0d1d63e709f5767cac5e6a6dfe8cbeb3a745aa
cona-dev <ycoqkr@gmail.com>
2020-07-10 13:31:31 +0900

    pickle, random Add

--- basic-syntax/built-in-function.py
+++ basic-syntax/built-in-function.py
@@ -1,7 +1,10 @@
-language = input("무슨 언어를 좋아하세요?")
-print("{}은 아주 좋은 언어입니다!".format(language))
+# language = input("무슨 언어를 좋아하세요?")
+# print("{}은 아주 좋은 언어입니다!".format(language))
 
 # dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
 print(dir())
 import random # 외장함수
+print(dir())
+
+import pickle
 print(dir())
