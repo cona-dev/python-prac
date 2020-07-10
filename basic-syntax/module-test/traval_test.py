aafd7a6a12c56548f5cecacf66389764da0f507f
cona-dev <ycoqkr@gmail.com>
2020-07-10 09:48:46 +0900

    module의 사용범위 및 공개범위를 직접 지정해서 사용하는 방법

---
+++ basic-syntax/module-test/traval_test.py
@@ -0,0 +1,7 @@
+# import traval.thailand # class, function은 직접 import 못함
+# trip_to = traval.thailand.ThailandPackage()
+# trip_to.detail()
+
+from traval import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
