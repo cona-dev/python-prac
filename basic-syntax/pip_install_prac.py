d540250edba2fc5908fbd065b271849bb18d33e7
cona-dev <ycoqkr@gmail.com>
2020-07-10 11:50:54 +0900

    pip install

---
+++ basic-syntax/pip_install_prac.py
@@ -0,0 +1,14 @@
+"""
+pip 를 사용하는 방법 
+https://pypi.org/
+pip install ~ 해서 라이브러리 설치
+ex) pip install beautifulsoup4
+
+Quick start에서 >>> 이 부분 복붙해서 사용
+"""
+
+from bs4 import BeautifulSoup
+#import BeautifulSoup
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+print (soup.prettify())
+
