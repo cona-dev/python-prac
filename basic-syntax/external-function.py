934b7f49b79951b587e34fe23571a1c4c1a74028
cona-dev <ycoqkr@gmail.com>
2020-07-10 13:45:06 +0900

    time, datetime, os function

--- basic-syntax/external-function.py
+++ basic-syntax/external-function.py
@@ -20,3 +20,19 @@ if os.path.exists(folder):
 else:
     os.makedirs(folder) #folder 생성
     print(folder, "폴더를 생성하였습니다.") #폴더가 있는데 또 생성하라고 했을 때, 구문 실행되지 않음
+
+
+print(os.listdir())
+
+# time : 시간 관련 함수
+import time 
+print(time.localtime())
+print(time.strftime("%Y-%m-%d %H:%M:%S")) #현재 시간 찍어내기
+
+import datetime
+print("오늘 날짜는 ", datetime.date.today())
+
+today = datetime.date.today() # 오늘 날짜 저장
+td = datetime.timedelta(days=100) #100일 저장
+
+print("우리가 만난지 100일은 ",today+td)
