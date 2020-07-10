3a72e35951629e039320c0464b90d94b705c0d54
cona-dev <ycoqkr@gmail.com>
2020-07-10 13:32:28 +0900

    random function guide check

--- basic-syntax/built-in-function.py
+++ basic-syntax/built-in-function.py
@@ -4,7 +4,10 @@
 # dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
 print(dir())
 import random # 외장함수
-print(dir())
+# print(dir())
 
-import pickle
-print(dir())
+# import pickle
+# print(dir())
+# 결과값으로 pickle과 random이 추가된 것을 terminal에서 확인할 수 있다.
+
+print(dir(random)) # random 모듈 안에서 사용할 수 있는 것들에 대한 내용을 확인할 수 있음
